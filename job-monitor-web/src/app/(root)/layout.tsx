65d3e1e0b8f4c6b7b941e3b634b75485094a0a01
Shubham Shinde <shubhamshinde225@gmail.com>
2026-07-03 22:02:18 +0530

    chore (web): Dummy table design created

--- job-monitor-web/src/app/(root)/layout.tsx
+++ job-monitor-web/src/app/(root)/layout.tsx
@@ -23,7 +23,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
           <div className="flex flex-1 flex-col">
             <div className="@container/main flex flex-1 flex-col gap-2">
               <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
-                <div className="px-4 lg:px-6">{children}</div>
+                <div className="">{children}</div>
               </div>
             </div>
           </div>
